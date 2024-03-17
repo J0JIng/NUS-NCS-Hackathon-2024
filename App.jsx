2b071a0c4baa4ac0901cde846d4ecbbbc781d4ff
pranavipea <pranavi11panduga@gmail.com>
2024-03-17 15:26:40 +0800

    routing!
    routing!

--- App.jsx
+++ App.jsx
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LandingPage from './components/LandingPage';
+import Providers from './components/Providers';
+import Users from './components/Users';
 
-export default function App() {
+
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.screen name = "LandingPage" component={LandingPage}/>
+        <Stack.screen name="Providers" component={Providers} />
+        <Stack.screen name="Users" component={Users} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
