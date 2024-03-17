f33107257e20482768522b3fe6a01b30f23cae9a
janelleenqi <110933149+janelleenqi@users.noreply.github.com>
2024-03-17 17:48:41 +0800

    pull

--- App.jsx
+++ App.jsx
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
+
 import React from 'react';
-=======
+
 
 import { useState } from 'react';
->>>>>>> refs/remotes/origin/jp
+
 import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
